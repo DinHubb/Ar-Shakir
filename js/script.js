f59498a6b742160ec3d4faeeea57d53ea4591fed
Ahliddin <89912128+DinHubb@users.noreply.github.com>
2023-02-02 14:00:31 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
-let input = document.querySelector('.todo-input');
-let list = document.querySelector('.todo-list');
-let priority = document.querySelector('.todo-priority');
-let form = document.querySelector('.todo-form');
-let upButton = document.querySelector('.up-button');
+let input = document.querySelector(".todo-input");
+let list = document.querySelector(".todo-list");
+let priority = document.querySelector(".todo-priority");
+let form = document.querySelector(".todo-form");
+let upButton = document.querySelector(".up-button");
 
-priority.onclick = function(){
-    priority.classList.toggle('is-important');
-    if (priority.classList.contains('is-important')){
-        priority.textContent = 'Важная задача';
-    }else {
-        priority.textContent = 'Повседневная задча';
-    }    
+priority.onclick = function () {
+  priority.classList.toggle("is-important");
+  if (priority.classList.contains("is-important")) {
+    priority.textContent = "Важная задача";
+  } else {
+    priority.textContent = "Повседневная задча";
+  }
 };
-form.onsubmit = function(evt) {
-    evt.preventDefault();
-let newTask = document.querySelector('li');
-newTask.textContent = input.value;
-if(priority.classList.contains('is-important')){
-    newTask.classList.add('is-important');
-};
-list.append(newTask);
-input.value = "";
+form.onsubmit = function (evt) {
+  evt.preventDefault();
+  let newTask = document.querySelector("li");
+  newTask.textContent = input.value;
+  if (priority.classList.contains("is-important")) {
+    newTask.classList.add("is-important");
+  }
+  list.append(newTask);
+  input.value = "";
 };
 
-window.onscroll = function (){
-    if(window.pageYOffset > 1000){
-        upButton.classList.add('shown');
-    } else {
-        upButton.classList.remove('shown');
-    }
-}
-upButton.onclick = function (){
-    window.scrollTo(0, 0);
-}
+window.onscroll = function () {
+  if (window.pageYOffset > 1000) {
+    upButton.classList.add("shown");
+  } else {
+    upButton.classList.remove("shown");
+  }
+};
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
